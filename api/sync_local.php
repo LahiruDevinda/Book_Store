9715d0d9e4d6218e6a4126a76fdb99de6f61ec53
Lahiru <devindalahiru2004@gmail.com>
2026-09-04 23:09:30 +0530

    Add initial implementation of sync_local API with user authentication check

---
+++ api/sync_local.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once __DIR__ . '/../config/db.php';
+startSecureSession();
+
+if (!isset($_SESSION['user']['userid'])) {
+    sendJsonResponse(['success' => false, 'message' => 'Authentication required.'], 401);
+}
+
+$userId = (int)$_SESSION['user']['userid'];
+$pdo = getDBConnection();
+
+$rawPayload = file_get_contents('php://input');
+$data = json_decode($rawPayload, true);
