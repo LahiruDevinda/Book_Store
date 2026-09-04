9c9112d1c88b29e42a78014f8ad67476a7fbc5e3
Lahiru <devindalahiru2004@gmail.com>
2026-09-04 22:59:57 +0530

    Add initial wishlist API implementation

---
+++ api/wishlist.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../config/db.php';
+startSecureSession();
+
+$pdo = getDBConnection();
+$userId = $_SESSION['user']['userid'] ?? null;
+
+$rawInput = file_get_contents('php://input');
+$input = json_decode($rawInput, true) ?: $_POST;
+$action = $input['action'] ?? ($_GET['action'] ?? 'get');
