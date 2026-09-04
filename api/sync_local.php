5ab5336dd4968bf608075c314939306f64536438
Lahiru <devindalahiru2004@gmail.com>
2026-09-04 23:09:54 +0530

    Fix handling of non-array payloads in sync_local API

--- api/sync_local.php
+++ api/sync_local.php
@@ -12,3 +12,7 @@ $pdo = getDBConnection();
 
 $rawPayload = file_get_contents('php://input');
 $data = json_decode($rawPayload, true);
+
+if (!is_array($data)) {
+    $data = ['guest_cart' => [], 'guest_wishlist' => []];
+}
