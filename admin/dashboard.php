7f2139126e2031a7702bfc4e9452c507e91ef614
LimeshDeSilva <limeshprabhashana357@gmail.com>
2026-09-04 22:44:26 +0530

    Add HTTP response code for unauthorized access in dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../config/db.php';
 startSecureSession();
 
 if (!isset($_SESSION['user']['userid']) || empty($_SESSION['user']['isAdmin'])) {
-  
+  http_response_code(403);
 }
 
 
