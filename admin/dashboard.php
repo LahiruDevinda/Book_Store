751321ffd173a5d11d8c18104308954be5041a4e
LimeshDeSilva <limeshprabhashana357@gmail.com>
2026-09-04 22:44:02 +0530

    Implement session management and access control in dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,13 @@
 <?php
 
-?>
+// Start the session and include necessary files
+require_once __DIR__ . '/../config/db.php';
+startSecureSession();
+
+if (!isset($_SESSION['user']['userid']) || empty($_SESSION['user']['isAdmin'])) {
+  
+}
+
+
+
+
